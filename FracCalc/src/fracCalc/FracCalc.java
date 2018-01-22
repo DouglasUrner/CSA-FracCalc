45777fa7efc8192c9b366f4a25241aa38c2c6aab
Douglas Urner <dlu@canishe.com>
2018-01-21 23:09:52 -0800

    Initial commit of pseudocode example.

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -2,33 +2,93 @@ package fracCalc;
 
 public class FracCalc {
 
-    public static void main(String[] args) 
-    {
-        // TODO: Read the input from the user and call produceAnswer() with an equation.
-    }
-    
-    /*
-     * IMPORTANT DO NOT DELETE OR CHANGE THE SIGNATURE OF THIS FUNCTION
-     *
-     * This function will be used to test your code.
-     *
-     * This function takes a String 'input' and produces the result
-     *
-     * input is a fraction string that needs to be evaluated.  For your
-     * program, this will be the user input.
-     *     e.g. input ==> "1/2 + 3/4"
-     *
-     * The function should return the result of the fraction after it
-     * has been calculated
-     *    e.g. return ==> "1_1/4"
-     */
-    public static String produceAnswer(String input)
-    { 
-        // TODO: Implement this function to produce the solution to the input.
-        
-        return "";
-    }
+  public static void main(String[] args) {
 
-    // TODO: Use the space below for any helper methods that you need.
-    
+    /**
+     * Loop processing input strings until we get told to quit.
+     * For each string:
+     * - Check if we're done.
+     * - Call produceAnswer().
+     * - Print the result.
+     */
+
+    // TODO: Read input from the user and pass it to produceAnswer().
+  }
+
+  /*
+   * IMPORTANT DO NOT DELETE OR CHANGE THE SIGNATURE OF THIS FUNCTION
+   *
+   * This function will be used to test your code.
+   *
+   * This function takes a String 'input' and produces the result
+   *
+   * input is a fraction string that needs to be evaluated.  For your
+   * program, this will be the user input.
+   *     e.g. input ==> "1/2 + 3/4"
+   *
+   * The function should return the result of the fraction after it
+   * has been calculated
+   *    e.g. return ==> "1_1/4"
+   */
+  static String produceAnswer(String input) {
+
+    /**
+     * Processing strategy:
+     * - Parse input into operand and operator tokens.
+     * - Work with one pair at a time (either a pair of operands from input and
+     *   their operator or the last result and a new operator and operand.
+     * - Break operands into component parts.
+     * - Reduce to a canonical fraction ([-]n/d, no whole part).
+     * - Apply operator to fractions.
+     * - Repeat if we haven't reached the end of the input.
+     * - Covert from the canoncal form to a proper reduced fraction.
+     *
+     * Throw an error if:
+     * - Initially there are more than two operands.
+     * - There is a leading or trailing operator.
+     * - There is an unknown operator or invalid operand.
+     */
+
+    // TODO: Implement this function to produce the solution to the input.
+
+    return input;
+  }
+
+  enum Part { whole, numerator, denominator }
+
+  /**
+   * parseNum() - return a portion of string representing a number.
+   *
+   * @param num - the String
+   * @param want - the part we want
+   * @return - the integer value of the part
+   */
+  private static int parseNum(String num, Part want) {
+    return 0;
+  }
+
+  /**
+   * checkOperator() - check that we got a valid operator.
+   *
+   * @param op
+   * @return
+   */
+  private static boolean checkOperator(String op) {
+    // Check that op is a String of length 1 from the set: + - / *.
+    // TODO: implement tests.
+    return true;
+  }
+
+  /**
+   * reduce() - a possibly improper fraction to proper form.
+   *
+   * @param n
+   * @param d
+   * @return
+   */
+  private static String reduce(int n, int d) {
+    // TODO: implement fraction reduction.
+    return n + "/" + d;
+  }
+
 }
