3d8f8ad7e5cf96ff323cf36ce9e64e86b846172f
Douglas Urner <dlu@canishe.com>
2018-02-07 17:59:07 -0800

    Example code and student guide (before conversion to Markdown).

---
+++ FracCalc/src/fracCalcExamples/FracCalc.java
@@ -0,0 +1,42 @@
+package fracCalcExamples;
+
+import java.util.Scanner;
+
+public class FracCalc {
+
+  public static void main(String[] args) {
+    // TODO: loop processing equations until user quits
+    // TODO: validate user input
+
+    // Manage the calculator
+    Scanner userInput = new Scanner(System.in);
+
+    // Get equations to process
+    String eq = userInput.nextLine();
+    // Calculate & print result
+  }
+
+  /*
+   * IMPORTANT DO NOT DELETE OR CHANGE THE SIGNATURE OF THIS FUNCTION
+   *
+   * This function will be used to test your code.
+   *
+   * This function takes a String 'input' and produces the result
+   *
+   * input is a fraction string that needs to be evaluated.  For your
+   * program, this will be the user input.
+   *     e.g. input ==> "1/2 + 3/4"
+   *
+   * The function should return the result of the fraction after it
+   * has been calculated
+   *    e.g. return ==> "1_1/4"
+   */
+  public static String produceAnswer(String input) {
+    // TODO: Implement this function to produce the solution to the input.
+
+    return "";
+  }
+
+  // TODO: Use the space below for any helper methods that you need.
+
+}
